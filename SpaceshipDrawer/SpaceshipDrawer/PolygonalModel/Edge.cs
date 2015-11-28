183ae783e0912750c925e468fbc845cdfe4656d1
Dmitry <dc914337@gmail.com>
2015-11-28 16:43:31 +0300

    adding Edges And Vertices

--- SpaceshipDrawer/SpaceshipDrawer/PolygonalModel/Edge.cs
+++ SpaceshipDrawer/SpaceshipDrawer/PolygonalModel/Edge.cs
@@ -8,7 +8,13 @@ namespace SpaceshipDrawer.PolygonalModel
 {
     class Edge
     {
-        public List<Vertex> Vertexes { get; set; } = new List<Vertex>();
+        public Vertex[] Vertexes { get; set; } = new Vertex[2];
+
+        public Edge(Vertex vertex1, Vertex vertex2)
+        {
+            Vertexes[0] = vertex1;
+            Vertexes[1] = vertex2;
+        }
 
     }
 }
--- SpaceshipDrawer/SpaceshipDrawer/PolygonalModel/Vertex.cs
+++ SpaceshipDrawer/SpaceshipDrawer/PolygonalModel/Vertex.cs
@@ -9,6 +9,22 @@ namespace SpaceshipDrawer.PolygonalModel
     class Vertex
     {
         public List<Edge> Edges { get; set; } = new List<Edge>();
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
 
+        public Vertex(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Edge AddVertex(Vertex nextVertex)
+        {
+            var newVertex = new Edge(this, nextVertex);
+            Edges.Add(newVertex);
+            return newVertex;
+        }
     }
 }
